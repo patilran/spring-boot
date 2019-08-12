2427211b48a53f8444cd25230d5ee689182f9a4c
Ranjit <ranjitpatil2301@gmail.com>
2019-08-12 14:47:31 +0530

    Added Project

---
+++ src/main/java/com/paramatrix/test/TestController.java
@@ -0,0 +1,15 @@
+package com.paramatrix.test;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@GetMapping("/test")
+	public ResponseEntity<Object> testApi(){
+		return new ResponseEntity<>("Api is working...!",HttpStatus.OK);
+	}
+}
---
+++ src/test/java/com/paramatrix/test/TestApplicationTests.java
@@ -0,0 +1,33 @@
+package com.paramatrix.test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(TestController.class)
+public class TestApplicationTests {
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void testController() throws Exception {
+		MockHttpServletResponse response = (MockHttpServletResponse) mvc
+				.perform(get("/test").contentType(MediaType.APPLICATION_JSON)).andReturn().getResponse();
+		assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
+	}
+}
